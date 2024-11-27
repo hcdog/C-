acdf2227a247ae7d881f44fbdcf860f419781596
hcdog <649124495@qq.com>
2024-11-27 21:58:41 +0800

    测试

---
+++ 测试1/小猪比体重/小猪比体重.cpp
@@ -0,0 +1,42 @@
+﻿// 小猪比体重.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+int main()
+{
+	int pig1 = 0;
+	int pig2=0, pig3 = 0;
+	std::cout << "请输入小猪1的体重" << std::endl;
+	std::cin >> pig1;
+	std::cout << "请输入小猪2的体重" << std::endl;
+	std::cin >> pig2;
+	std::cout << "请输入小猪2的体重" << std::endl;
+	std::cin >> pig2;
+	if (pig1 > pig2&&pig1 > pig3) {
+		std::cout << "pig1是最重的" << std::endl;
+		return 0;
+	}
+	if (pig2 > pig1&&pig2 > pig3) {
+		std::cout << "pig2是最重的" << std::endl;
+		return 0;
+
+	}
+	if (pig3 > pig2&&pig3 > pig1) {
+		std::cout << "pig3是最重的" << std::endl;
+		return 0;
+	}
+	std::cout << "比不出来" << std::endl;
+	return 0;
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
