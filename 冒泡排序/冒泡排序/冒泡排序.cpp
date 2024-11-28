2e6f13d6510b469326a8efe636f0a48eca69d45f
hcdog <649124495@qq.com>
2024-11-28 16:28:30 +0800

    冒泡排序

---
+++ 冒泡排序/冒泡排序/冒泡排序.cpp
@@ -0,0 +1,22 @@
+﻿// 冒泡排序.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+int main()
+{
+	int b =0;
+	int arr[7] = { 1,2,34,56,7,41,4};
+	for (int i = 0; i < 6; i++) {
+		for (int j = 0; j < 6; j++) {
+			if (arr[j] < arr[j + 1]){
+				b = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = b;
+			}
+		}
+	}
+	for (int k = 0; k < 7; k++) {
+		std::cout << arr[k] << " ";
+	}
+}
